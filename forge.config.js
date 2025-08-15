df6271c64013cc75880ffbdf539482b77ca504ae
juhaogang <473247597@qq.com>
2025-08-15 15:28:04 +0800

    测试

--- forge.config.js
+++ forge.config.js
@@ -44,4 +44,17 @@ module.exports = {
       [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'github-user-name',
+          name: 'github-repo-name'
+        },
+        prerelease: false,
+        draft: true
+      }
+    }
+  ]
 };
