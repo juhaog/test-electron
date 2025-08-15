51d2b788aac44656e96d8a3f128e7731865e5b23
juhaogang <473247597@qq.com>
2025-08-15 14:52:13 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow, ipcMain } = require('electron/main')
+// import { app, BrowserWindow } from "electron";
+const path = require('node:path');
+// import path from 'node:path';
+// const path = require('path');
+// import path from "path";
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  ipcMain.handle('ping', () => 'pong')
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
